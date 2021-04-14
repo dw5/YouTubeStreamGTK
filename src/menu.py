7722ed1629b1a78705aad3f73052815cc3817e98
Todd Weaver <todd@puri.sm>
2021-04-14 16:40:43 -0700

    Properly assigning copyright to Purism, SPC

--- src/menu.py
+++ src/menu.py
@@ -1,6 +1,6 @@
 # menu.py
 #
-# Copyright 2021 Todd Weaver
+# Copyright 2021 Purism, SPC
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
